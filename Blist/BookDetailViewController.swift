7e5318f303345626b1e46bc1f9323682d25e2076
加賀谷諒 <ryokagaya0624@gmail.com>
2018-03-30 16:14:34 +0900

    アプリがあれば開き、なけらばWebページを開く

--- Blist/BookDetailViewController.swift
+++ Blist/BookDetailViewController.swift
@@ -26,7 +26,6 @@ class BookDetailViewController: UIViewController {
         detailView.publishedDateLabel.text = model.book.publishedDate
         detailView.pageCountLabel.text = String(model.book.pageCount)
         detailView.explanationArea.text = model.book.explanation
-        
         detailView.buyRakutenButton.addTarget(self, action: #selector(openRakutenApp), for: .touchUpInside)
         detailView.buyAmazonButton.addTarget(self, action: #selector(openAmazonApp), for: .touchUpInside)
     }
@@ -34,7 +33,13 @@ class BookDetailViewController: UIViewController {
     @objc private func openAmazonApp() {
         Alert(title: "Amazonで開きます", message: "AmazonのAppに遷移します")
             .addAction(title: "OK", style: .default) { (action) in
-
+                let url = URL(string: "http://www.amazon.co.jp/dp/\(self.model.book.ISBN_10)")!
+                let appScheme = URL(string: "com.amazon.mobile.shopping://")!
+                if UIApplication.shared.canOpenURL(appScheme) {
+                    UIApplication.shared.open(appScheme)
+                } else {
+                    UIApplication.shared.open(url)
+                }
             }
             .open()
     }
@@ -42,7 +47,13 @@ class BookDetailViewController: UIViewController {
     @objc private func openRakutenApp() {
         Alert(title: "楽天で開きます", message: "楽天のAppに遷移します")
             .addAction(title: "OK", style: .default) { (action) in
-    
+                let url = URL(string: "https://books.rakuten.co.jp/search/dt?g=001&bisbn=\(self.model.book.ISBN_10)")!
+                let appScheme = URL(string: "Rakuten://")!
+                if UIApplication.shared.canOpenURL(appScheme) {
+                    UIApplication.shared.open(appScheme)
+                } else {
+                    UIApplication.shared.open(url)
+                }
             }
             .open()
     }
