4f16a537e29902fc2567390882018bf2b9858d34
加賀谷諒 <ryokagaya0624@gmail.com>
2018-03-30 15:11:49 +0900

    Buttonに仮のTargeを当てる

--- Blist/BookDetailViewController.swift
+++ Blist/BookDetailViewController.swift
@@ -26,7 +26,27 @@ class BookDetailViewController: UIViewController {
         detailView.publishedDateLabel.text = model.book.publishedDate
         detailView.pageCountLabel.text = String(model.book.pageCount)
         detailView.explanationArea.text = model.book.explanation
+        
+        detailView.buyRakutenButton.addTarget(self, action: #selector(openRakutenApp), for: .touchUpInside)
+        detailView.buyAmazonButton.addTarget(self, action: #selector(openAmazonApp), for: .touchUpInside)
+    }
     
+    @objc private func openAmazonApp() {
+        Alert(title: "Amazonで開きます", message: "AmazonのAppに遷移します")
+            .addAction(title: "OK", style: .default) { (action) in
+                let view = self.view as! QRReaderView
+                view.start()
+            }
+            .open()
+    }
+    
+    @objc private func openRakutenApp() {
+        Alert(title: "楽天で開きます", message: "楽天のAppに遷移します")
+            .addAction(title: "OK", style: .default) { (action) in
+                let view = self.view as! QRReaderView
+                view.start()
+            }
+            .open()
     }
     
 }
